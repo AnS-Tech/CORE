71b5a1d759fdfdf61cc7941628e31382bee4e3cc
Evilis Glenio <glenio.developer@gmail.com>
2023-09-22 07:53:53 -0300

    Adcionando detalhes aos botões de Ação

--- src/components/ActionButton/index.tsx
+++ src/components/ActionButton/index.tsx
@@ -1,25 +1,40 @@
-import React from "react";
-import { CartContainer, CartIcon } from "./styles";
-import { ActionButtonProps, CartContainerProps } from "./interfaces";
+import React, { useState } from "react";
+import { CartContainer, CartContainerWithHover, CartIcon } from "./styles";
+import { ActionButtonProps } from "./interfaces";
 import cartIcon from "src/styles/svgs/cartIcon.svg";
 import wishList from "src/styles/svgs/wishList.svg";
 import quickView from "src/styles/svgs/quickView.svg";
+import whiteCartIcon from "src/styles/svgs/whiteCartIcon.svg";
+import { colors } from "src/styles/tokens";
+import { any, set } from "zod";
 
 export const ActionButton: React.FC<ActionButtonProps> = ({
-  status = "cartIcon",
+  status = null,
+  cartContainerColor = colors.grayScale100,
 }) => {
   const icon = {
     cartIcon,
     wishList,
     quickView,
+    whiteCartIcon,
+  };
+  const [hoverMouseCart, setHoverMouseCart] = useState<any>();
+
+  const HoverOnMouseEnter = () => {
+    setHoverMouseCart(icon.whiteCartIcon);
+  };
+  const HoverOnMouseLeave = () => {
+    setHoverMouseCart(icon.cartIcon);
   };
 
   return status === "cartIcon" ? (
-    <CartContainer
-      style={{ position: "absolute", bottom: "2.2rem", right: "1rem" }}
+    <CartContainerWithHover
+      onMouseEnter={HoverOnMouseEnter}
+      onMouseLeave={HoverOnMouseLeave}
+      cartContainerColor={cartContainerColor}
     >
-      <CartIcon src={icon[status]} width={20} height={20} alt={`${status}`} />
-    </CartContainer>
+      <CartIcon src={hoverMouseCart} width={20} height={20} alt={`${status}`} />
+    </CartContainerWithHover>
   ) : (
     <CartContainer style={{ marginBottom: "0.5rem" }}>
       <CartIcon src={icon[status]} width={20} height={20} alt={`${status}`} />
--- src/components/ActionButton/styles.ts
+++ src/components/ActionButton/styles.ts
@@ -1,21 +1,32 @@
 import { styled, css } from "styled-components";
 import { CartContainerProps } from "./interfaces";
 import Image from "next/image";
+import { colors } from "src/styles/tokens";
 
-export const CartContainer = styled.button<CartContainerProps>`
+export const CartContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 43px;
+  height: 43px;
+  border-radius: 2rem;
+
+  background-color: ${colors.white};
+  border: none;
+  cursor: pointer;
+`;
+
+export const CartContainerWithHover = styled(CartContainer)<CartContainerProps>`
+  position: absolute;
+  bottom: 2.2rem;
+  right: 1rem;
   ${({ cartContainerColor }) => css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    width: 43px;
-    height: 43px;
-    border-radius: 2rem;
-
     background-color: ${cartContainerColor};
-    border: none;
-    cursor: pointer;
   `}
+  &:hover {
+    background-color: ${colors.success};
+  }
 `;
 
 export const CartIcon = styled(Image)``;
