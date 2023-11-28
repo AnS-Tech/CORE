0b9bf23f6b782f89355b5f7ca985bd7bb97542af
Evilis Glenio <glenio.developer@gmail.com>
2023-11-28 10:49:07 -0300

    Inicialização do Menu Section Botton

--- src/components/Header/sections/Bottom/index.tsx
+++ src/components/Header/sections/Bottom/index.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
 import * as S from "./styles";
+import { FaPhoneVolume } from "react-icons/fa6";
 
 export const Bottom = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.Body>
+        <S.MenuList>
+          <S.MenuOption>
+            <Link href="/">Flores</Link>
+          </S.MenuOption>
+          <S.MenuOption>
+            <Link href="/">Buquês</Link>
+          </S.MenuOption>
+          <S.MenuOption>
+            <Link href="/">Ocasiões</Link>
+          </S.MenuOption>
+          <S.MenuOption>
+            <Link href="/">Ofertas</Link>
+          </S.MenuOption>
+          <S.MenuOption>
+            <Link href="/">Monte seu Arranjo</Link>
+          </S.MenuOption>
+        </S.MenuList>
+
+        <S.CallNow>
+          <FaPhoneVolume />
+          <Link href="tel:+5584991745274">84 99174-5274</Link>
+        </S.CallNow>
+      </S.Body>
+    </S.Container>
+  );
 };
