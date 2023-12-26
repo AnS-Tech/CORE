49fceed8af957c15f266229dc3d8f4bdeb16a1ad
Evilis Glenio T. Gomes <glenio.developer@gmail.com>
2023-12-26 16:36:52 -0300

    Feat header bottom (#14)
    * Create of the Footer Section
    
    * Created a Logo Component
    
    * update of the MainFooter
    
    * Conteúdos alocados de forma dinamica
    
    * Inicialização da responsividade do footer
    
    * Mobile Main Footer: Menu pronto, falta as opções do menu. Mobile DownFooter: Ajustes de posicionamento quando a tela ficar menor. (@media)
    
    * Footer responsivo finalizado
    
    * Header middle destop + Header middle resposivo(em andamento)
    
    * Inicialização do Menu Section Botton
    
    * Atualização do Menu Botton: Conteúdo Dinâmico e Tipado.
    
    * Atualização de menu
    
    * Retirada dos links do Main Footer e do modulo Mobile
    
    * Criação do Hamburguer para acomodação dos Menus
    
    * Now, input-width is auto
    
    * Update of our header botton, including new options menus

--- src/components/Header/sections/Bottom/content.tsx
+++ src/components/Header/sections/Bottom/content.tsx
@@ -1,6 +1,34 @@
 import Link from "next/link";
 import * as S from "./styles";
 
+export const MenuContent: Array<MenuBottonProps> = [
+  {
+    index: 1,
+    href: "/",
+    menu: "Flores",
+  },
+  {
+    index: 2,
+    href: "/",
+    menu: "Buquês",
+  },
+  {
+    index: 3,
+    href: "/",
+    menu: "Ocasiões",
+  },
+  {
+    index: 4,
+    href: "/",
+    menu: "Ofertas",
+  },
+  {
+    index: 5,
+    href: "/",
+    menu: "Monte seu Arranjo",
+  },
+];
+
 export const BContent: React.FC<MenuBottonProps> = ({ href, index, menu }) => {
   return (
     <S.MenuOption key={index}>
