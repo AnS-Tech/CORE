00ffb172dee150879c2f6375dfe3412434a616ca
João P M Morais <paulomedeiros5000@gmail.com>
2023-12-27 21:40:20 -0300

    correções de tipagem

--- src/components/Product/Product.stories.tsx
+++ src/components/Product/Product.stories.tsx
@@ -14,11 +14,16 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     status: "",
-    productName: "Rosa do Deserto",
-    productPrice: "99.90",
     priceColor: colors.grayScale900,
     textColor: colors.grayScale700,
     backgroundColor: colors.white,
     sizeStatus: "small",
+    product: {
+      id: "1",
+      description: "Product Description",
+      name: "Product Name",
+      price: "1000",
+      image: ["https://picsum.photos/500/500"],
+    },
   },
 };
